5be4f1cd6cb4604601dd5e95e6f314f85d97fbaf
sulaimanbosu <mang.mp01@gmail.com>
2022-05-11 14:05:42 +0700

    update 11-05

--- android/app/src/app-center-post-clone.sh
+++ android/app/src/app-center-post-clone.sh
@@ -8,7 +8,7 @@ set -x
 
 cd ..
 
-git clone -b stable https://github.com/flutter/flutter.git
+git clone -b stable https://github.com/SulaimanBosu/Taywin_project.git
 export PATH=`pwd`/flutter/bin:$PATH
 
 flutter channel stable
